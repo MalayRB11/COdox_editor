31f48459cac01d9dd1f12cf807ae54a337e7d2b6
Malay Raj Bareth <malayraj2003@gmail.com>
2025-03-24 13:37:27 +0530

    initial commit

---
+++ docufy_latest/src/app/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+    return (
+        <div className = "flex justify-center min-h-screen items-center ">
+            This is a DOCUMENT PAGE!
+        </div>
+    );
+}
+
+export default DocumentsPage;
