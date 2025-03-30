a03d62fd66678ce7f3fcd983e18920579de6cbc0
yashsinha501 <13yashsinha@gmail.com>
2025-03-30 14:37:44 +0530

    error resolving

--- docufy_latest/src/app/documents/[documentId]/page.tsx
+++ docufy_latest/src/app/documents/[documentId]/page.tsx
@@ -8,7 +8,8 @@ interface DocumentIdPageProps {
 
 
 const DocumentsIdPage = async ({ params }: DocumentIdPageProps) =>{
-    const {documentId} = await params;
+    // const {documentId} = await params;
+    await params;
     return(
         <div className="min-h-screen bg-[#FAFBFD]">
             <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
